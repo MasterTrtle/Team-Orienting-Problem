243b2bea096f9842902bd8224194aacdcfb86800
SelectiveBamboo <58808879+SelectiveBamboo@users.noreply.github.com>
2022-12-27 21:38:52 +0100

    Changes to perform analysis over all the instances

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from os import listdir
-from parser import read_map_file
+from os import listdir, path
+from parser import read_map_file, listFilesInDir
 from genetic_Solver import create_graph,genetic_algorithm
 from graphic_util import print_path
 
@@ -11,12 +11,29 @@ if __name__ == '__main__':
     #     for instance in listdir('Instances/' + set_folder):
     #         # read the map file
     #         map_data = read_map_file('Instances/' + set_folder + '/' + instance)
-    #         instances += [map_data]
+    #         instances += [map_data]   
+    startingDirectories = ["Instances"]
+    files = []
 
-    map_data = read_map_file('Instances/Set_21_234/p2.2.b.txt')
-    g1 = create_graph(map_data[0])
+    gs = []
+    solutions = []
+
+    print("Listing files")
+    files = listFilesInDir(startingDirectories)
+
+    print(files)
 
 
-    solution = genetic_algorithm(g1, 2, 10)
-    print_path(g1, solution)
+    for file in files:
+        print("Currently on file : " + file)
+        #map_data = read_map_file('Instances/Set_21_234/p2.2.b.txt')
+        map_data = read_map_file(file)
+        g1 = create_graph(map_data[0])
 
+
+        
+        solution = genetic_algorithm(g1, 2, 10)
+        print_path(g1, solution)
+
+        gs.append(g1)
+        solutions.append(solution)
