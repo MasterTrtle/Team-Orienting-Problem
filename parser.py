7a869b16e6ee422767d2d7c5fffecd7e66f1af7b
SelectiveBamboo <58808879+SelectiveBamboo@users.noreply.github.com>
2022-12-27 21:41:47 +0100

    changes to list all instances in directory

--- parser.py
+++ parser.py
@@ -1,3 +1,28 @@
+from os import listdir, path
+
+ignoreList = [".ipynb", "README", ".nis", ".blb", ".ris"]
+
+def shouldIgnoreFile(fileName):
+    for i in ignoreList:
+        if i in fileName:
+            return True
+    
+    return False
+
+
+
+def listFilesInDir(dirs):
+    listFiles = []
+    for dir in dirs:
+        for f in listdir(dir):
+            if path.isfile(dir+'/'+f) and not shouldIgnoreFile(f):
+                listFiles.append(dir+'/'+f)
+            elif path.isdir(dir+'/'+f):
+                listFiles += listFilesInDir([dir+'/'+f])
+    
+    return listFiles
+
+
 def read_map_file(file_path):
     # Open the file and read the contents
     with open(file_path, 'r') as f:
@@ -25,17 +50,18 @@ def read_map_file(file_path):
     # map is a rectangle, lenght = max x, width = max y
     map = [[0 for i in range(max([x[1] for x in map_data]) + 1)] for j in range(max([x[0] for x in map_data]) + 1)]
 
+    for i in map_data[1:-1]:
+        map[i[0]][i[1]] = i[2]
+
     if map_data[0][0] == map_data[-1][0] and map_data[0][1] == map_data[-1][1]:
         #if starting and ending point are the same
         map[map_data[0][0]][map_data[0][1]] = "B"
     else:
         # the starting point is the first point in the map_data
         map[map_data[0][0]][map_data[0][1]] = "S"
+
         # the ending point is the last point in the map_data
         map[map_data[-1][0]][map_data[-1][1]] = "E"
-
-    for i in map_data[1:-1]:
-        map[i[0]][i[1]] = i[2]
 
     # Return the map data
     return map, num_vertices, num_paths, max_time_per_path
